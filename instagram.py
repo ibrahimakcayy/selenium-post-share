0cc910cc711557ba7ed2133253755718b7ec9487
ibrahim <57787419+ibrahimakcayy@users.noreply.github.com>
2024-04-03 23:45:27 +0300

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -27,19 +27,28 @@ def openurl(url):
     print("Webpage opened")
 
 
+#get xpaths from notepad
+def xpaths(name,paths):
+    f=open(f"{name}.txt","r")
+    raw_xpaths=list(f.read().split("\n"))
+    f.close()
+    b=[i.split(":")[1] for i in raw_xpaths]
+    return(b[paths])
+
+
 #login instagram
 def login(usern,passw):
     time.sleep(1)
     
     #write username
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input').send_keys(usern)
+    driver.find_element(By.XPATH,xpaths("i-xpath",0)).send_keys(usern)
     
     #write password
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input').send_keys(passw)
+    driver.find_element(By.XPATH,xpaths("i-xpath",1)).send_keys(passw)
     time.sleep(1)
 
     #click login button
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button').click()
+    driver.find_element(By.XPATH,xpaths("i-xpath",2)).click()
     time.sleep(10)
 
     print("Logged in")
@@ -48,35 +57,34 @@ def login(usern,passw):
     time.sleep(8)
     
     #click notification decline
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]').click()
+    driver.find_element(By.XPATH,xpaths("i-xpath",3)).click()
     time.sleep(2)
-
 
 #share photo in instagram 
 def share_photo(text,photo_url,usern):
     
     print("Start Sharing")
 
-    #open share popup
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[7]/div').click()
+    #open create popup
+    driver.find_element(By.XPATH,xpaths("i-xpath",4)).click()
     time.sleep(3)
     
     #upload photo
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div[1]/form/input').send_keys(photo_url)
+    driver.find_element(By.XPATH,xpaths("i-xpath",5)).send_keys(photo_url)
     time.sleep(5)
     
     #share actions
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div').click()
+    driver.find_element(By.XPATH,xpaths("i-xpath",6)).click()
     time.sleep(2)
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div').click()
+    driver.find_element(By.XPATH,xpaths("i-xpath",6)).click()
     time.sleep(3)
     
     #write caption
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div[1]').send_keys(text)
+    driver.find_element(By.XPATH,xpaths("i-xpath",7)).send_keys(text)
     time.sleep(0.5)
     
-    #share photo
-    driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div').click()
+    #share actions
+    driver.find_element(By.XPATH,xpaths("i-xpath",6)).click()
     time.sleep(15)
 
     print("Post Shared")
